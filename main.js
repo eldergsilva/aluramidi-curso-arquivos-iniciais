4f213a60db34227f3cac48eef94d18ab1f604707
unknown <eldergsilva@gmail.com>
2024-03-28 13:08:56 -0300

    alterada a funcionalidade while para for para lista de teclas

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 function tocaSom  (idElementoAudio){
     document.querySelector(idElementoAudio).play();
-} 
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador< listaDeTeclas.length;contador++) {
     const tecla = listaDeTeclas[contador]
     const  instrumento = tecla.classList[1] 
     const idAudio = `#som_${instrumento}`
 
     tecla.onclick = function (){
         tocaSom(idAudio)
-    };
-    contador ++;
-     
+    }; 
+
+    tecla.onkeydown() = function(){
+        tecla.classList.add('ativa')
+
+    }
+    tecla.onkeyup() = function(){
+        tecla.classList.remove('ativa')
+
+    }
+
 }
-
-
- 
