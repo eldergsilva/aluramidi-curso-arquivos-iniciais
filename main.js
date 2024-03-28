1d6cee5f38c7af8cd89d15f2ec9206df6ea17f3f
unknown <eldergsilva@gmail.com>
2024-03-28 10:17:46 -0300

    criado a funcao tocaSom para receber os id´s

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function tocaSomPom  (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom  (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 } 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
 
+while (contador < listaDeTeclas.length) {    
+
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador ++;
+    console.log(contador);
+}
 
 
  
